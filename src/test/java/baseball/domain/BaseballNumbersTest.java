b60fb54004d14b73086b19844b882010db8ae59d
jaehyun <wogusrla77@naver.com>
2025-05-21 21:53:56 +0900

    test: : 사용자 정상입력, 입력예외 테스트 추가

--- src/test/java/baseball/domain/BaseballNumbersTest.java
+++ src/test/java/baseball/domain/BaseballNumbersTest.java
@@ -2,6 +2,8 @@ package baseball.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaseballNumbersTest {
@@ -11,5 +13,17 @@ class BaseballNumbersTest {
         BaseballNumbers baseballNumbers = BaseballNumbers.generateRandom();
         int size = baseballNumbers.getNumbers().size();
         assertEquals(3, size);
+    }
+
+    @Test
+    void 사용자_정상_입력_테스트() {
+        BaseballNumbers numbers = BaseballNumbers.of("123");
+        assertEquals(List.of(1, 2, 3), numbers.getNumbers());
+    }
+    @Test
+    void 사용자_입력_예외_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> BaseballNumbers.of("113")); // 중복
+        assertThrows(IllegalArgumentException.class, () -> BaseballNumbers.of("12")); // 잘못된 자리수 입력
+        assertThrows(IllegalArgumentException.class, () -> BaseballNumbers.of("abc")); // 숫자가 아님
     }
 }
