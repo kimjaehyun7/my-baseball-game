4cb70e7bd4a93b783284207e740d70c4a2e0b244
jaehyun <wogusrla77@naver.com>
2025-05-21 23:01:11 +0900

    feat: 사용자 입력 값 파싱 및 검증 기능 구현

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -1,6 +1,9 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BaseballNumbers {
     private final List<Integer> numbers;
@@ -10,7 +13,22 @@ public class BaseballNumbers {
     }
 
     public static BaseballNumbers of(String input) {
-        return null;
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리수를 입력해야합니다.");
+        }
+        List<Integer> numbers = new ArrayList<>();
+        Set<Integer> uniqueCheck = new HashSet<>(); // 중복 체크용 Set
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+            }
+            int number = c - '0';
+            if (!uniqueCheck.add(number)) {
+                throw new IllegalArgumentException("중복된 숫자는 허용되지 않습니다.");
+            }
+            numbers.add(number);
+        }
+        return new BaseballNumbers(numbers);
     }
 
     public static BaseballNumbers generateRandom() {
