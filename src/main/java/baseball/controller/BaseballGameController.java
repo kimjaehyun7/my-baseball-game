13faa861f8e5746b6aad1da4303901b4de846e45
jaehyun <wogusrla77@naver.com>
2025-05-23 18:54:59 +0900

    feat: Controllllerler 구현

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,7 +1,45 @@
 package baseball.controller;
 
+import baseball.domain.BallCount;
+import baseball.domain.BaseballNumbers;
+import baseball.domain.Umpire;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class BaseballGameController {
 
     public void run() {
+
+        while (true) {
+            System.out.println("===============숫자야구 게임===============");
+            System.out.println("규칙 : 서로 다른 3자리 숫자를 입력해야 합니다.");
+            BaseballNumbers randomNumbers = BaseballNumbers.generateRandom();
+
+            boolean success = false;
+            while (!success) {
+                try {
+                    game(randomNumbers);
+                    success = true;
+                } catch (IllegalArgumentException e) {
+                    System.out.println("[예외발생] " + e.getMessage());
+                    System.out.println("숫자를 다시 입력하세요.");
+                }
+            }
+            if (InputView.reStart().equals("2")) {
+                break;
+            }
+        }
+    }
+
+    private static void game(BaseballNumbers randomNumbers) {
+        Umpire umpire = new Umpire(randomNumbers);
+        BallCount result;
+        do {
+            BaseballNumbers inputNumbers = BaseballNumbers.of(InputView.readInput());
+            result = umpire.compare(inputNumbers);
+            OutputView.printResult(result);
+        } while (!result.isThreeStrike());
+
+        OutputView.endGame();
     }
 }
