3a6fc06706520e532b5d59ce75ee58033e1a32cf
jaehyun <wogusrla77@naver.com>
2025-05-23 17:13:38 +0900

    feat: InputView, OutputView 클래스 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,8 +1,17 @@
 package baseball.view;
 
+import java.util.Scanner;
+
 public class InputView {
 
+    private static final Scanner scanner = new Scanner(System.in);
     public static String readInput() {
-        return null;
+        System.out.print("숫자를 입력해주세요: ");
+        return scanner.nextLine();
+    }
+
+    public static String reStart() {
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ");
+        return scanner.nextLine();
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,14 @@
 package baseball.view;
 
+import baseball.domain.BallCount;
+
 public class OutputView {
 
-    public static void printResult() {
+    public static void printResult(BallCount result) {
+        System.out.println(result);
+    }
+
+    public static void endGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
